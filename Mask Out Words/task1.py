0ddcbe7ad356f4f845a9d43246c6072ea2ea2521
elinaKaneva <elina.kaneva@gmail.com>
2015-02-17 23:35:26 +0200

    removed test prints

--- Mask Out Words/task1.py
+++ Mask Out Words/task1.py
@@ -2,10 +2,8 @@ import re
 
 
 def maskOutWords(words, text):
-
     txt = re.split("(\W+)", text)
     txt_low = [x.lower() for x in txt]
-    print(txt_low)
 
     for word in words:
         while word.lower() in txt_low:
@@ -13,11 +11,6 @@ def maskOutWords(words, text):
             asterisk = "*" * len(word)
             txt_low[index] = asterisk
             txt[index] = asterisk
-            print(txt_low)
-            print(txt)
 
     result = "".join(txt)
-    print(result)
     return result
-
-maskOutWords(["E", "jorko", "super"], "Jorko e super e!")
