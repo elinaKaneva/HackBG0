11d7fff49bf28e7defb4bd32706bb24d811dae64
elinaKaneva <elina.kaneva@gmail.com>
2015-02-21 23:13:17 +0200

    task 3 csv read and reverse

--- Query Language/task3.py
+++ Query Language/task3.py
@@ -1,1 +1,13 @@
-task3.py
+db = {}
+
+with open("data.csv") as f:
+    temp_data = ([line.strip('\n\r').split(',') for line in f.readlines()])
+    db_data = list(zip(*temp_data))
+
+print(db_data)
+
+print("Welcome, dummy!")
+query = input("Please enter your quiery:")
+print(query)
+
+#print(db.keys())
